960e0ade5474532d38f6f8a5cbbe409ce6dbb5d7
Karolberlowski <Karol.berlowski@gmail.com>
2021-07-03 10:27:12 +0200

    first commit

---
+++ src/components/data.jsx
@@ -0,0 +1,20 @@
+export const initialMems = [
+  {
+    img: 'https://i.wpimg.pl/730x0/m.gadzetomania.pl/haha-7d43148228848200c1ccc59676b.jpg',
+    upvote: 10,
+    downvote: 1,
+    id: 3,
+  },
+  {
+    img: 'https://img28.dmty.pl//uploads/202106/1624028771_n7yeba_600.jpg',
+    upvote: 2,
+    downvote: 2,
+    id: 2,
+  },
+  {
+    img: 'https://img21.demotywatoryfb.pl//uploads/202104/1619542560_onrhhj_600.jpg',
+    upvote: 3,
+    downvote: 0,
+    id: 1,
+  },
+];
